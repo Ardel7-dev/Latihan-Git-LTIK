03128a26490be97af20f67c3ceffe0c807185565
Tsabitah Ardelia Wahyudi <tsabitah365@gmail.com>
2025-11-05 11:04:30 +0700

    Create Main.py
    C1

---
+++ Main.py
@@ -0,0 +1,10 @@
+koocheng = {
+    "nama": "Mochi",
+    "umur": 2,
+    "ras": "kampung",
+    "betina": True,
+    "hobi": ("memanjat pohon", "menggigit"),
+    "status": "meningsoy"
+}
+
+print(koocheng)
